b52682dbbae8fafda91abb715b04a3a4c9dc419a
salman <salmankn033@gmail.com>
2022-09-24 01:36:59 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,40 @@
+import {View, ActivityIndicator, SafeAreaView} from 'react-native';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
+import {WebView} from 'react-native-webview';
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  const ActivityIndicatorElement = () => {
+    //making a view to show to while loading the webpage
+    return (
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100%',
+          width: '100%',
+        }}>
+        <ActivityIndicator color="#fed034" size="large" />
+      </View>
+    );
+  };
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <WebView
+        source={{uri: 'https://fritochicken.com/'}}
+        javaScriptEnabled={true}
+        //For the Cache
+        domStorageEnabled={true}
+        //View to show while loading the webpage
+        renderLoading={() => <ActivityIndicatorElement />}
+        //Want to show the view or not
+        startInLoadingState={true}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default App;

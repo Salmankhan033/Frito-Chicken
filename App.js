ebed63e5a24b3524fa6ad2a94a7298f89f06d5be
Salman <salman@Salmans-MacBook-Pro.local>
2023-03-13 01:46:17 +0500

    ios 1.0.0

--- App.js
+++ App.js
@@ -3,18 +3,55 @@ import {
   ActivityIndicator,
   SafeAreaView,
   StatusBar,
+  BackHandler,
+  Platform,
+  ImageBackground,
   Text,
-  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import SplashScreen from 'react-native-splash-screen';
 import {WebView} from 'react-native-webview';
 
+import NetInfo from '@react-native-community/netinfo';
+import {Colors} from './assets/Colors';
+
 const App = () => {
+  const [isConnected, setConnected] = useState(true);
+  const [refresh, setRefresh] = useState(false);
+
   StatusBar.setBarStyle('light-content', true);
+  const webViewRef = useRef(null);
+  const onAndroidBackPress = () => {
+    if (webViewRef.current) {
+      webViewRef.current.goBack();
+      return true; // prevent default behavior (exit app)
+    }
+    return false;
+  };
+
   useEffect(() => {
     SplashScreen.hide();
+    if (Platform.OS === 'android') {
+      BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
+      return () => {
+        BackHandler.removeEventListener(
+          'hardwareBackPress',
+          onAndroidBackPress,
+        );
+      };
+    }
   }, []);
+  useEffect(() => {
+    const netInfroSubscribe = NetInfo.addEventListener(state => {
+      setConnected(state.isConnected);
+      // if (!state.isConnected) {
+      //   alert('No connection');
+      // }
+    });
+    return netInfroSubscribe;
+  }, [refresh]);
   const ActivityIndicatorElement = () => {
     //making a view to show to while loading the webpage
     return (
@@ -25,34 +62,128 @@ const App = () => {
           height: '100%',
           width: '100%',
         }}>
-        <ActivityIndicator color="#36d1fd" size="large" />
+        <ActivityIndicator color={Colors.App_Color} size="large" />
       </View>
     );
   };
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        backgroundColor: '#000',
-      }}>
-      <StatusBar barStyle="light-content" backgroundColor="#000" />
-      <WebView
-        source={{uri: 'www.gethooptie.com/'}}
-        javaScriptEnabled={true}
-        //For the Cache
-        domStorageEnabled={true}
-        //View to show while loading the webpage
-        renderLoading={() => <ActivityIndicatorElement />}
-        //Want to show the view or not
-        startInLoadingState={true}
-        // style={{
-        //   marginTop: 20,
-        //   width: Dimensions.get('window').width,
-        //   height: Dimensions.get('window').height,
-        // }}
-      />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <SafeAreaView style={{flex: 0, backgroundColor: Colors.App_Color}} />
+      <SafeAreaView style={styles.container}>
+        <StatusBar
+          barStyle={isConnected ? 'light-content' : 'dark-content'}
+          backgroundColor={isConnected ? Colors.App_Color : Colors.white}
+        />
+        {!isConnected && (
+          <View style={styles.noNetView}>
+            <ImageBackground
+              imageStyle={styles.iconBg}
+              source={require('./assets/no_internet.png')}
+              style={styles.imageStye}>
+              <View style={styles.noConnectView}>
+                <View style={styles.noConnectTextView}>
+                  <Text style={styles.noConnectText}>Ooops!</Text>
+                  <Text style={styles.noConnectSimpleText}>
+                    No internet connection found.
+                  </Text>
+                  <Text style={styles.noConnectSimpleText}>
+                    Check your connection and try again
+                  </Text>
+                </View>
+                <TouchableOpacity
+                  style={styles.btnStyle}
+                  onPress={() => setRefresh(!refresh)}>
+                  <Text style={styles.btnText}>Try again</Text>
+                </TouchableOpacity>
+              </View>
+            </ImageBackground>
+          </View>
+        )}
+        {isConnected && (
+          <WebView
+            // source={{uri: 'https://app.socar.dev/'}}
+            source={{uri: 'https://gethooptie.com/'}}
+            javaScriptEnabled={true}
+            ref={webViewRef}
+            mediaPlaybackRequiresUserAction={false}
+            //For the Cache
+            domStorageEnabled={true}
+            //View to show while loading the webpage
+            renderLoading={() => <ActivityIndicatorElement />}
+            //Want to show the view or not
+            startInLoadingState={true}
+            geolocationEnabled={true}
+            scalesPageToFit
+            allowsInlineMediaPlayback={true}
+            javaScriptEnabledAndroid={true}
+            allowFileAccess={true}
+            originWhitelist={['*']}
+          />
+        )}
+      </SafeAreaView>
+    </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.white,
+  },
+  noNetView: {
+    backgroundColor: Colors.white,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 10,
+    height: '100%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageStye: {
+    height: '100%',
+    width: '100%',
+  },
+  iconBg: {
+    height: '100%',
+    width: '100%',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    resizeMode: 'contain',
+  },
+  noConnectView: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    flex: 1,
+  },
+  noConnectTextView: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  noConnectText: {
+    color: Colors.textColor,
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginVertical: '2%',
+  },
+  noConnectSimpleText: {
+    color: Colors.textColor,
+    fontSize: 18,
+  },
+  btnStyle: {
+    backgroundColor: Colors.App_Color,
+    paddingVertical: '3.5%',
+    paddingHorizontal: '6%',
+
+    marginTop: '30%',
+  },
+  btnText: {
+    color: Colors.white,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
 
 export default App;
---
+++ assets/Colors.js
@@ -0,0 +1,8 @@
+export const Colors = {
+  App_Color: '#38d1fd',
+  white: '#ffffff',
+  black: '#000',
+  textColor: '#929497',
+  btnColor: '#565656',
+  bgColor: '#d0d2d3',
+};
